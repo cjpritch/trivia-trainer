0ba8fd5f174626afb1bda0f63d0893b7b6f6b36e
Kamadulski999 <104084465+Kamadulski999@users.noreply.github.com>
2022-06-17 05:43:40 -0500

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,27 @@
-$('.dropdown-trigger').dropdown();
+var questionNumber = 10
+var questionCategory
+
+
+var questionRequest = function() {
+
+    // loop through this function for each question
+    for(i=0; i < questionNumber; i++){
+
+    // generate a random positive or negative number
+    var plusOrMinus = (Math.random() - 0.5) * 2
+
+    //if number is positive use API-Ninja if negative use Jservice
+
+    if(plusOrMinus>0) {
+        //API-Ninja request
+        console.log("APININJA")
+       
+    }else {
+        // JService request
+        console.log("Jservice")
+    }
+    
+ }   
+}
+
+questionRequest()
