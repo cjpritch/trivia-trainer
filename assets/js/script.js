af0e204586683cb2067ef58eb6bcc541ed6f75aa
Kamadulski999 <104084465+Kamadulski999@users.noreply.github.com>
2022-06-18 12:05:54 -0500

    fixed unicode formatting

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,14 @@
 //
-// these two will be the information coming back from the dropdowns
+// these two variables are placeholders for the information coming back from the dropdowns (category & # of questions)
 var questionNumber = 50
+var questionCategory = "sciencenature"
 
-var questionCategory = "artliterature"
-
-// create variables to be used below
+// create global variables to be used below
 var question
 var answer
+var categoryCounter
+var questionCounter = 0
+var questionArray = []
 
 var questionRequest = function() {
 
@@ -30,12 +32,19 @@ var questionRequest = function() {
         success: function(result) {       
         question = (result[0].question)
         answer = (result[0].answer)  
-        // we'll need to call the function to display the question here passing question and answer into it as arguments
 
+        //send question to questionArray
+        dataObj = {
+            question: question,
+            answer: answer
+        }    
+        
+        pushToArray(dataObj)     
+       
         // console.log("NINJA")
         // console.log("Question: " + question)
         // console.log("Answer: " + answer)    
-         
+                 
         },
         error: function ajaxError(jqXHR) {
             console.error('Error: ', jqXHR.responseText);
@@ -44,106 +53,124 @@ var questionRequest = function() {
        
     }else {
 
-    // openTDB request
+    // convert NinjaAPI category query values to OpenTDB query values
+    categoryConverter(questionCategory)    
 
-     var openTDBRequest = fetch("https://opentdb.com/api.php?amount=1").then(function(openTDBRequest){
+    // openTDB request
+     var openTDBRequest = fetch("https://opentdb.com/api.php?amount=1&category="+categoryCounter).then(function(openTDBRequest){
         openTDBRequest.json().then(function(data){
         question = (data.results[0].question)
         answer = (data.results[0].correct_answer)
         
     // add text to T/F questions
         if(answer == "True" || answer == "False"){
-            question = "True or False? " + question
-        }
-    
-         // we'll need to call the function to display the question here passing question and answer into it as arguments
+            question = "True or False? " + question       
+         }
+
+    //fix unicode formatting
+        answer = answer.replaceAll(/&quot;/g,"'")
+        question = question.replaceAll(/&quot;/g,"'")
+
+        answer = answer.replaceAll(/&amp;;/g,"&")
+        question = question.replaceAll(/&amp;;/g,"&")
+
+        answer = answer.replaceAll(/&#039;/g,"'")
+        question = question.replaceAll(/&#039;/g,"'")
+
+    //send question to questionArray
+        dataObj = {
+            question: question,
+            answer: answer
+        }    
+        pushToArray(dataObj)       
+          
         // console.log("OpenTDB")
         // console.log("Question: " + question)
-        // console.log("Answer: " + answer)    
-        
-        categoryConverter(questionCategory)
-            
-            
-
-         })
-        .catch(function(error) {
-            console.log("Houston we have a problem")
-            
+        // console.log("Answer: " + answer)                         
         })
-    });
-                            
-     };
-  
-}
-   
- }   
 
-   // convert NinjaAPI categories to OpenTDB categories
+        .catch(function(error) {
+            console.log("Houston we have a problem")            
+        })
+    });                            
+   };    
+  }  
+}   
+
+
+// we'll call the function to display the question here. I had to add a delay so that there is time
+// for the results to return and so that it's not looking at an empty array
+// we'll need to link this to a questionCounter 
+
+console.log(questionArray)
+var test = function() {
+   {        console.log(questionArray[questionCounter].question)
+            console.log(questionArray[questionCounter].answer)
+    }  
+}
+delayThis = setTimeout(test, 500);
+
+
+
+   // this function converts NinjaAPI categories to OpenTDB categories
    var categoryConverter = function(questionCategory) {
     switch(questionCategory) {
     case "general":
-    questionCategory = "9"
-    console.log(questionCategory)
+    categoryCounter = "9"    
     break;
 
     case "artliterature":
-    questionCategory = "25"
-    console.log(questionCategory)
+    categoryCounter = "25"    
     break;
 
     case "language":
-    questionCategory = "10"
-    console.log(questionCategory)
+    categoryCounter = "10"
     break;
     
     case "sciencenature":
-    questionCategory = "17"
-    console.log(questionCategory)
+    categoryCounter = "17"
     break;
 
     case "peopleplaces":
-    questionCategory = "26"
-    console.log(questionCategory)
+    categoryCounter = "26"
     break;
 
     case "geography":
-    questionCategory = "22"
-    console.log(questionCategory)
+    categoryCounter = "22"
     break;
 
     case "historyholidays":
-    questionCategory = "23"
-    console.log(questionCategory)
+    categoryCounter = "23"
     break;
 
     case "entertainment":
-    questionCategory = "14"
-    console.log(questionCategory)
+    categoryCounter = "14"
     break;
 
     case "toysgames":
-    questionCategory = "16"
-    console.log(questionCategory)
+    categoryCounter = "16"
     break;
 
     case "music":
-    questionCategory = "12"
-    console.log(questionCategory)
+    categoryCounter = "12"
     break;
 
     case "religionmythology":
-    questionCategory = "20"
-    console.log(questionCategory)
+    categoryCounter = "20"
     break;
 
     case "sportsliesure":
-    questionCategory = "21"
-    console.log(questionCategory)
+    categoryCounter = "21"
     break;         
 
-    default: 
-    console.log(questionCategory)
-}}
+    default: console.log("no category")
+    };
+}
 
-questionRequest()
+var pushToArray = function(dataObj) {
+    questionArray.push(dataObj)
+
+}
+    questionRequest()
+    console.log()
 $('.dropdown-trigger').dropdown();
